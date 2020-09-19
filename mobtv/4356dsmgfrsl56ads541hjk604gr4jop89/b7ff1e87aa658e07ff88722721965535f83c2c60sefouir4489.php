09da89d5a027b951ceaf61f36eb1a9be087cdec5
footballplus <62063221+footballplus@users.noreply.github.com>
2020-09-19 20:12:05 +0100

    Update b7ff1e87aa658e07ff88722721965535f83c2c60sefouir4489.php

--- mobtv/4356dsmgfrsl56ads541hjk604gr4jop89/b7ff1e87aa658e07ff88722721965535f83c2c60sefouir4489.php
+++ mobtv/4356dsmgfrsl56ads541hjk604gr4jop89/b7ff1e87aa658e07ff88722721965535f83c2c60sefouir4489.php
@@ -7,6 +7,9 @@
 
 
 
+{"A":"03","B":"download the new version ======>>To be able to use the application, you need to update
+","C":"1005","D":"NEW VERSION","E":"update required","F":"https://icons.iconarchive.com/icons/dtafalonso/android-lollipop/512/Downloads-icon.png","G":"http://www.mediafire.com/file/f0boyrkez5a71ip/football_plus+New+.apk/file","H":"openbrowser","I":"false","J":"direct","K":"","L":"","M":"","R":"","N":"","O":"03.05.2019 16:32:35","P":0},
+
 
 
 {"A":"03","B":"Our Telegram Group","C":"1005","D":"Our Telegram Group","E":"Reporting of non-working channels","F":"https://1.bp.blogspot.com/-7vi-miz6tSc/XWseU6ipwbI/AAAAAAAAATc/qqvWZ8xswQ0YzCW4VCC4cBarOgE4aSIhwCLcBGAs/s1600/PicsArt_09-01-03.20.32.png","G":"https://t.me/footballiveplus","H":"openbrowser","I":"false","J":"direct","K":"","L":"","M":"","R":"","N":"","O":"03.05.2019 16:32:35","P":0},
